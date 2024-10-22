3c7b611475f7193d154da88b02dd35d95f0ca943
Kishore <kishorlearnings@gmail.com>
2024-10-22 18:20:34 +0530

    Factory Pattern commit

---
+++ FactoryPattern/src/com/learnings/PDFDocument.java
@@ -0,0 +1,11 @@
+package com.learnings;
+
+public class PDFDocument implements Document {
+
+	@Override
+	public void open() {
+		System.out.println("PDF document is opened");
+
+	}
+
+}
---
+++ FactoryPattern/src/com/learnings/TextDocument.java
@@ -0,0 +1,11 @@
+package com.learnings;
+
+public class TextDocument implements Document {
+
+	@Override
+	public void open() {
+		System.out.println("Text document is opened");
+
+	}
+
+}
